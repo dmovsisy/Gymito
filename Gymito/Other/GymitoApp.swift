d3195e061901b6febff39a51d01c06b463194124
dmovsisy <dmovsisy@uwaterloo.ca>
2024-12-10 16:19:03 -0500

    Refactoring

--- Gymito/Other/GymitoApp.swift
+++ Gymito/Other/GymitoApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct GymitoApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
