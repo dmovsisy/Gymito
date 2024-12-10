f37bd5b3a00d697ba50666c54befefe81c07939e
dmovsisy <dmovsisy@uwaterloo.ca>
2024-12-10 13:40:37 -0500

    Initial Commit

---
+++ Gymito/GymitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymitoApp.swift
+//  Gymito
+//
+//  Created by David Movsisyan  on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct GymitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymitoTests/GymitoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymitoTests.swift
+//  GymitoTests
+//
+//  Created by David Movsisyan  on 2024-12-10.
+//
+
+import Testing
+@testable import Gymito
+
+struct GymitoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
