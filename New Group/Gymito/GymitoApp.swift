e5149b0fb00edf90c281c36f55ede529a7091249
dmovsisy <dmovsisy@uwaterloo.ca>
2024-12-10 15:11:14 -0500

    adding firebase

---
+++ New Group/Gymito/GymitoApp.swift
@@ -0,0 +1,21 @@
+//
+//  GymitoApp.swift
+//  Gymito
+//
+//  Created by David Movsisyan  on 2024-12-10.
+//
+
+import SwiftUI
+import FirebaseCore
+
+@main
+struct GymitoApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
