1edd81c07fd13346a083a6b5d638c73a36937473
dmovsisy <dmovsisy@uwaterloo.ca>
2024-12-12 01:51:18 -0500

    Made the registration screen + basic validation for log iin

---
+++ Gymito/Views/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  Gymito
+//
+//  Created by David Movsisyan  on 2024-12-12.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
---
+++ Gymito/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Gymito
+//
+//  Created by David Movsisyan  on 2024-12-10.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
